8cf3a6c8111e39a94d6fc7fc42b02490946f043e
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sweet: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- device.mk
+++ device.mk
@@ -129,6 +129,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom
