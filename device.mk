a4c596909dfb01124dec74c20d5b0ab5350b2ad3
Arian <arian.kulmer@web.de>
2021-03-14 18:21:54 +0100

    sweet: Configure partitions

--- device.mk
+++ device.mk
@@ -3,7 +3,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
