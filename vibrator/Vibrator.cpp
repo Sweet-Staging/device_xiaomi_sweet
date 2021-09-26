f4b32645f7804b9c1b67b6dc53fe6697e3ce9f93
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-09-26 10:27:11 +0530

    sweet: vibrator: rebrand to sweet
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "vendor.qti.vibrator"
+#define LOG_TAG "vendor.qti.vibrator.sweet"
 
 #include <dirent.h>
 #include <inttypes.h>
